76d39cbef417cc9aa61396b09592f42e238a17b4
Melissa Zuengler <melissazuengler@Melissas-MacBook-Pro.local>
2022-08-23 23:28:11 -0500

    Complete

--- functions_practice.py
+++ functions_practice.py
@@ -4,4 +4,22 @@ def hello():
 def pack(par_1,par_2,par_3):
     return print([par_2,par_2,par_3])
 
-def eat_lunch():
+
+def eat_lunch(lunch_list:list):
+    if len(lunch_list) == 0:
+        print("My lunchbox is empty")
+        return
+    for i in range(len(lunch_list)):
+        if i == 0:
+            print("First I eat " + lunch_list[i])
+        else:
+            print(f"Next I eat {lunch_list[i]}")
+    
+
+    # Easiest way to loop through
+    # for x in lunch_list:
+    #     print(x)
+
+l = ["Apple","Pizza","Soup","Pie"]
+
+eat_lunch(l)
